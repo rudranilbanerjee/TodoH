1dff20c36fe581fa043d86b6c83029084cec9083
Rudranil Banerjee <105772744+rudranilbanerjee@users.noreply.github.com>
2023-04-06 19:36:53 +0530

    Delete Todo-List/src directory

--- Todo-List/src/store.js
+++
@@ -1,5 +0,0 @@
-import rootReducer from "./reducers/index";
-import { applyMiddleware,legacy_createStore as createStore } from "redux";
-import thunk from "redux-thunk";
-const store=createStore(rootReducer,{},applyMiddleware(thunk));
-export default store;
